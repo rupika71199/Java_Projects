6237e81da52125e057d1e1ba3b56fab502747b25
Rupika Srinivasan <54763077+rupika71199@users.noreply.github.com>
2023-08-19 09:41:22 +0530

    Delete BiteSpeed1 directory

--- BiteSpeed1/src/main/java/com/example/BiteSpeed/Repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.BiteSpeed.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.BiteSpeed.Entity.ContactEntity;
-
-public interface ContactRepository extends JpaRepository<ContactEntity, Integer>{
-
-}
